133d007e4b92da46396a3dc4a2a913c4bab0f548
Paritosh Kumar <kumar.paritosh20@gmail.com>
2020-03-30 15:07:33 +0530

    addded documentation in stylesheets.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NavBar from './components/CommonComponents/NavBar/NavBar';
 import BackgroundCurtain from './components/BackgroundComponent/BackgroundCurtains';
 import './styling/Layout.css';
-import './styling/Text.css';
+import './styling/Typography.css';
 import './App.css';
 
 function App() {
